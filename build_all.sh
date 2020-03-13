b2c67f313ade8ae74d67f107959818e424cea273
Mabel Zhang <mabel@openrobotics.org>
2020-03-13 17:08:39 -0400

    combine two bash scripts

--- build_all.sh
+++ build_all.sh
@@ -3,13 +3,25 @@
 # Mabel Zhang
 # 24 Apr 2019
 #
+# Clone repositories if do not exist locally.
 # Update and rebuild all Ignition repositories
 #
 
 THIS_DIR=`pwd`
 
+DISTRO=blueprint
+
 # Path on local machine where repos are located
-ROOT=/data/ws/sim/ign
+if [[ "$DISTRO" == blueprint ]]; then
+  ROOT=/data/ws/sim/ign
+elif [[ "$DISTRO" == dome ]]; then
+  ROOT=/data/ws/sim/ign_dome
+fi
+
+# If ROOT doesn't exist, create it.
+if [[ ! -d "$ROOT" ]]; then
+  mkdir -p $ROOT
+fi
 
 # Usage: $ echo -e "$MAGENTA"abc"$ENDC"abc""
 OKCYAN="\e[96m"
@@ -34,24 +46,46 @@ declare -a repos=(
   "ign-gazebo"
   "ign-launch")
 
-# Branches from https://bitbucket.org/osrf/gazebodistro/src/default/collection-blueprint.yaml
-declare -a branches=(
-  "ign-cmake2"
-  "ign-math6"
-  "ign-common3"
-  "ign-tools0"
-  "ign-msgs4"
-  "ign-transport7"
-  "sdf8"
-  "ign-plugin1"
-  "ign-physics1"
-  "ign-rendering2"
-  "ign-sensors2"
-  "ign-fuel-tools3"
-  "ign-gui2"
-  "ign-gazebo2"
-  "ign-launch1"
-)
+# Repos are in the order of dependency for compilation
+if [[ "$DISTRO" == blueprint ]]; then
+  # Branches from https://bitbucket.org/osrf/gazebodistro/src/default/collection-blueprint.yaml
+  declare -a branches=(
+    "ign-cmake2"
+    "ign-math6"
+    "ign-common3"
+    "ign-tools0"
+    "ign-msgs4"
+    "ign-transport7"
+    "sdf8"
+    "ign-plugin1"
+    "ign-physics1"
+    "ign-rendering2"
+    "ign-sensors2"
+    "ign-fuel-tools3"
+    "ign-gui2"
+    "ign-gazebo2"
+    "ign-launch1"
+  )
+elif [[ "$DISTRO" == dome ]]; then
+  # Branches from https://bitbucket.org/osrf/gazebodistro/src/default/collection-dome.yaml
+  declare -a branches=(
+    "ign-cmake2"
+    "ign-math6"
+    "ign-common3"
+    "ign-tools1"
+    "ign-msgs5"
+    "ign-transport8"
+    "sdf9"
+    "ign-plugin1"
+    "ign-physics2"
+    "default"
+    "default"
+    "ign-fuel-tools4"
+    "default"
+    "default"
+    "ign-launch1"
+  )
+fi
 
 for i in "${!repos[@]}"
 do
@@ -59,10 +93,32 @@ do
   branch="${branches[$i]}"
 
   echo -e "$OKCYAN""$repo""$ENDC"
-  echo -e "$OKCYAN""branch: "`hg branch`"$ENDC"
-  cd $ROOT/$repo/build
-  hg pull
+
+  # If fresh checkout
+  if [[ ! -d "$ROOT/$repo" ]]; then
+    cd $ROOT
+    if [[ "$repo" == sdformat ]]; then
+      hg clone https://bitbucket.org/osrf/sdformat
+    else
+      hg clone https://bitbucket.org/ignitionrobotics/$repo
+    fi
+    cd $ROOT/$repo
+  # If repo already exist locally
+  else
+    cd $ROOT/$repo
+    hg pull
+  fi
+
   hg update $branch
+  echo -e "$OKCYAN""branch: "`hg branch`"$ENDC"
+
+  if [[ ! -d "$ROOT/$repo/build" ]]; then
+    mkdir $ROOT/$repo/build
+    cd $ROOT/$repo/build
+    cmake ..
+  else
+    cd $ROOT/$repo/build
+  fi
   make -j16
   sudo make install
 done
